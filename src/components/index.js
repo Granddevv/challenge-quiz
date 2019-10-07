df94f6e8cc2f991288d83b24f1160c5a169e6305
granddevv <spacingflight@outlook.com>
2019-10-06 20:52:38 -0600

    Register Quiz Header, Footer, Content, Score

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import QuizHeader from './quiz-header';
+import QuizContent from './quiz-content';
+import QuizFooter from './quiz-footer';
+import QuizScore from './quiz-score';
+
+export { QuizHeader, QuizFooter, QuizContent, QuizScore }
